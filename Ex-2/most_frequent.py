cb2e72316fcd0337a20338acdd3f054f698e3bb6
Shuddown <shudheshkibhakt@gmail.com>
2024-05-09 17:20:06 +0530

    merging done

--- Ex-2/most_frequent.py
+++ Ex-2/most_frequent.py
@@ -10,8 +10,4 @@ def most_frequent(string: str) -> list[str]:
 
 
 test = input("Give a string: ")
-<<<<<<< HEAD
 print(most_frequent(test))
-=======
-print(most_frequent(test))
->>>>>>> origin/master
