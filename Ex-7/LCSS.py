e940199e7d11eae64132e54b252e5863e0c1ebae
Shuddown <shudheshkibhakt@gmail.com>
2024-06-03 09:16:57 +0530

    finished

--- Ex-7/LCSS.py
+++ Ex-7/LCSS.py
@@ -12,28 +12,22 @@ def lcs_recursive(X, Y, m = -1, n = -1):
     else:
         return max(lcs_recursive(X, Y, m, n-1), lcs_recursive(X, Y, m-1, n))
 
-
-def lcs(X, Y):
+def lcs(X,Y):
     m = len(X)
     n = len(Y)
+    L = [[0 for _ in range(m+1)] for _ in range(n+1)]
+    for i in range(n):
+        for j in range(m):
+            if(Y[i] == X[j]): L[i+1][j+1] = min(L[i][j+1], L[i+1][j]) + 1
+            else: L[i+1][j+1] = max(L[i][j+1], L[i+1][j])
     
-    L = [[0 for _ in range(n+1)] for _ in range(m+1)]
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            if X[i-1] == Y[j-1]:
-                L[i][j] = L[i-1][j-1] + 1
-            else:
-                L[i][j] = max(L[i-1][j], L[i][j-1])
-    
-    return L[m][n]
-
-X = "AGGTAB"
-Y = "GXTXAYB"
-print("Length of LCS is", lcs(X, Y))
-print("Length of LCS is", lcs_recursive(X, Y))
+    return L[n][m]
 
 
+if __name__ == "__main__":
+    X = "AGGTAB"
+    Y = "GXTGAYB"
+    print(lcs_recursive(X,Y))
+    print(lcs(X,Y))
+            
 
-
-X = "AGGTAB"
-Y = "GXTXAYB"
